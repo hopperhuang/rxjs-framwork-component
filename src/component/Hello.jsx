2e641c2fc0a25f8fa777c9aed8b38bcf801fe552
hopperhuang <huanjiwe@126.com>
2017-07-26 16:03:34 +0800

    增加了Model方法，用于定义data模型,subsribesm模型和subjects模型.

--- src/component/Hello.jsx
+++ src/component/Hello.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
-import Rx from 'rxjs/Rx';
+import Connect from './Connect';
 
-const Subject = Rx.Subject;
-const listSubject = new Subject();
-const initListData = ['abc', 'def'];
-const outerSubscribe = listSubject.map(message => initListData.push(message));
 function List(props) {
   const list = props.list;
   const listeItem = list.map(item => <div>{item}</div>);
@@ -16,34 +12,29 @@ function List(props) {
   );
 }
 
+
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
     this.addItem = this.addItem.bind(this);
-    this.state = {};
-  }
-  componentWillMount() {
-    const listData = initListData;
-    this.state.list = listData;
-    const setState = this.setState.bind(this);
-    outerSubscribe.subscribe(() => setState({ list: initListData }));
-  }
-  shouldComponentUpdate() {
-    return true;
   }
   addItem() {
-    console.log(this);
-    listSubject.next('u clicke me???');
+    const { dispatch } = this.props;
+    dispatch.next('you click me??');
   }
   render() {
-    const list = this.state.list;
+    const { initData } = this.props;
     return (
       <div>
-        <Button onClick={this.addItem}>click me to add item</Button>
-        <List list={list} />
+        <Button onClick={this.addItem}>click me to add person</Button>
+        <List list={initData} />
       </div>
     );
   }
 }
+const todoListModel = {
+  initData: ['abc', 'def'],
+  dataChangeMethod: (subject, initData) => subject.map(message => initData.push(message)),
+};
 
-export default TodoList;
+export default Connect(todoListModel)(TodoList);
---
+++ src/component/Rxdux.jsx
@@ -0,0 +1,31 @@
+const firstModel = { person: {
+  name: 'mike',
+  age: 20,
+  hobby: {
+    sports: 'footabll',
+    entertament: 'ktv',
+  },
+} };
+const secondModel = {
+  nation: {
+    name: 'china',
+    area: 960,
+    capital: {
+      province: 'beijing',
+    },
+  },
+};
+const thirdModel = {
+  school: {
+    name: 'scau',
+    age: '110',
+    location: {
+      province: 'guangdong',
+      city: 'guangzhou',
+    },
+  },
+};
+
+function makeSubject(models) {
+
+}
